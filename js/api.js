e360bc99d811251f1fc3de16c1e63d304c799a0d
MoNeN-92 <63068358+MoNeN-92@users.noreply.github.com>
2022-08-31 05:00:40 +0400

    first commit

---
+++ js/api.js
@@ -0,0 +1,86 @@
+
+$(function () {
+
+    $(".home").click(function (e) {
+        e.preventDefault();
+$(".loading").removeClass("d-none");
+        $.ajax({
+            type: "get",
+            url: "Data.json",
+            dataType: "json",
+            success: function (response) {
+                response.forEach(Data => {
+                    let show = `${Data.feedbackHome}`;
+                    $(".loading").addClass("d-none");
+                    $('#hom').append(show);
+                });
+
+            }
+        });
+
+    })
+});
+$(function () {
+
+    $(".pr").click(function (e) {
+        e.preventDefault();
+        $(".loading").removeClass("d-none");
+        $.ajax({
+            type: "get",
+            url: "Data.json",
+            dataType: "json",
+            success: function (response) {
+                response.forEach(Data => {
+                    let shows = `${Data.feedbackProduction}`;
+                    $(".loading").addClass("d-none");
+                    $('#pro').append(shows);
+                });
+
+            }
+        });
+
+    })
+});
+$(function () {
+
+    $(".ab").click(function (e) {
+        e.preventDefault();
+        $(".loading").removeClass("d-none");
+        $.ajax({
+            type: "get",
+            url: "Data.json",
+            dataType: "json",
+            success: function (response) {
+                response.forEach(Data => {
+                    let showes = `${Data.feedbackAbout}`;
+                    $(".loading").addClass("d-none");
+                    $('#abo').append(showes);
+                });
+
+            }
+        });
+
+    })
+});
+$(function () {
+
+    $(".cont").click(function (e) {
+        e.preventDefault();
+        $(".loading").removeClass("d-none");
+        $.ajax({
+            type: "get",
+            url: "Data.json",
+            dataType: "json",
+            success: function (response) {
+                response.forEach(Data => {
+                    let showes = `${Data.feedbackContact}`;
+                    $(".loading").addClass("d-none");
+                    $('#cont').append(showes);
+                    
+                });
+
+            }
+        });
+
+    })
+});
